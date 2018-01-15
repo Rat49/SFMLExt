00b15a8b9986c246d711e9b96f3b5c0c374cbefe
Rat49 <rat49.xt@gmail.com>
2018-01-15 23:50:59 +0300

    WIP

--- SFMLExt/Cycle/cycle.h
+++ SFMLExt/Cycle/cycle.h
@@ -8,8 +8,10 @@ public:
 	void addDrawable();
 	void removeDrawable();
 
+	void setView(const sf::View& view);
+	const sf::View& getView() const;
+
 private:
 	std::vector<sf::Drawable> m_subscribers;
-	
-
+	sf::RenderWindow m_window;
 };
--- SFMLExt/Cycle/mainwindow.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class MainWindow
-{
-public:
-
-
-};
-
