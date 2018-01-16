1bdc4868ca651a1dd65e29bb1140185aebaf819b
Rat49 <rat49.xt@gmail.com>
2018-01-16 16:10:26 +0300

    WIP. still no good model to continue

--- SFMLExt/Cycle/cycle.cpp
+++ SFMLExt/Cycle/cycle.cpp
@@ -1,1 +1,95 @@
+#include "cycle.h"
+#include <tuple>
 
+using namespace ext;
+
+Cycle::Cycle(std::weak_ptr<sf::RenderWindow> wnd)
+    : m_window(wnd)
+{
+}
+
+void Cycle::registerGameObject(GameObject* obj)
+{
+    if(obj == nullptr)
+    {
+        return;
+    }
+
+    if(obj->isFixedUpdateOverriden())
+    {
+        m_toFixedUpdate.push_back(obj);
+    }
+    if(obj->isUpdateOverriden())
+    {
+        m_toUpdate.push_back(obj);
+    }
+
+    auto renderable = dynamic_cast<sf::Drawable*>(obj);
+    if(renderable != nullptr)
+    {
+        m_toRender.push_back(renderable);
+
+        std::sort(std::begin(m_toRender), std::end(m_toRender), [](GameObject* left, GameObject* right)
+                  {
+                      auto lLayer = left->getSpriteLayer();
+                      auto rLayer = right->getSpriteLayer();
+                      return (lLayer != rLayer) ? (lLayer < rLayer) : (left->getSpriteOrder() < right->getSpriteOrder());
+                  });
+    }
+}
+
+void Cycle::unregisterGameObject(GameObject* obj)
+{
+    if(obj == nullptr)
+    {
+        return;
+    }
+
+    if(obj->isFixedUpdateOverriden())
+    {
+        m_toFixedUpdate.erase(std::remove(std::cbegin(m_toFixedUpdate), std::cend(m_toFixedUpdate), obj), std::cend(m_toFixedUpdate));
+    }
+    if(obj->isUpdateOverriden())
+    {
+        m_toUpdate.erase(std::remove(std::cbegin(m_toUpdate), std::cend(m_toUpdate), obj), std::cend(m_toUpdate));
+    }
+    if(dynamic_cast<sf::Drawable*>(obj) != nullptr)
+    {
+    m_toRender.erase(std::remove(std::cbegin(m_toRender), std::cend(m_toRender), obj), std::cend(m_toRender));
+}
+
+void Cycle::runSync()
+{
+    execute();
+}
+
+void Cycle::runAsync()
+{
+    //start and return
+}
+
+void Cycle::execute()
+{
+}
+
+void Cycle::update()
+{
+}
+
+void Cycle::fixedUpdate()
+{
+}
+
+void Cycle::render() const
+{
+    if(m_window.expired())
+    {
+        return;
+    }
+
+    auto wnd = m_window.lock();
+    for(auto obj : m_toRender)
+    {
+        wnd->render(obj);
+    }
+}
--- SFMLExt/Cycle/cycle.h
+++ SFMLExt/Cycle/cycle.h
@@ -1,17 +1,63 @@
 #pragma once
-
+#include <memory>
 #include <SFML\Graphics.hpp>
 
-class Cycle final
+namespace ext
 {
-public:
-	void addDrawable();
-	void removeDrawable();
 
-	void setView(const sf::View& view);
-	const sf::View& getView() const;
+    class GameObject
+    {
+    public:
+        bool isUpdateEnabled() const { return m_updateEnabled; }
+        bool isFixedUpdateEnabled() const { return m_fixedUpdateEnabled; }
+        bool IisRenderEnabled() const { return m_renderEnabled; }
+        sf::Uint32 getSpriteLayer() const { return m_spriteLayer; }
+        sf::Uint32 getSpriteOrder() const { return m_spriteOrderInLayer; }
 
-private:
-	std::vector<sf::Drawable> m_subscribers;
-	sf::RenderWindow m_window;
-};
+        virtual void update(float deltaTime) {}
+        virtual void fixedUpdate(float deltaTime) {}
+
+        bool isUpdateOverriden() const
+        {
+            return GameObject::update == this->update;
+        }
+
+        bool isFixedUpdateOverriden() const
+        {
+            return GameObject::fixedUpdate == this->fixedUpdate;
+        }
+
+    protected:
+        bool m_updateEnabled;
+        bool m_fixedUpdateEnabled;
+
+        //to activate render you need to inherit sf::Drawable
+        bool m_renderEnabled;
+        sf::Uint32 m_spriteLayer;
+        sf::Uint32 m_spriteOrderInLayer;
+    };
+
+    class Cycle final
+    {
+    public:
+        Cycle(std::weak_ptr<sf::RenderWindow> wnd);
+
+        void registerGameObject(GameObject* obj);
+        void unregisterGameObject(GameObject* obj);
+
+        void runSync();
+        void runAsync();
+
+    private:
+        void execute();
+        void update() const;
+        void fixedUpdate() const;
+        void render() const;
+
+    private:
+        std::vector<GameObject*> m_toUpdate;
+        std::vector<GameObject*> m_toFixedUpdate;
+        std::vector<sf::Drawable*> m_toRender;
+        std::weak_ptr<sf::RenderWindow> m_window;
+    };
+}
